46fd1362ff103ca25d65e4efe70f697b39c78add
thebend <bdavidson@NNC-015.nnc.local>
2015-11-24 13:46:55 -0800

    Renamed "Data Analysis" -> analysis, added collection dir with NHL icetracker proof of concept
    Short consistent dirs "analysis", "collection" for data analysis, data
    collection.
    
    icetracker dir with sample HTML, script to parse a given game ID and
    output formatted play-by-play.  Sample output also included.

---
+++ collection/nhl-icetracker/icetracker.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+
+class IceTrackerRow():
+	def __init__(self):
+		pass
+
+def getIceTrackerData(gameId):
+	url = 'http://www.nhl.com/gamecenter/en/icetracker?id=%s' % gameId
+
+	#parse html
+	html = requests.get(url).text
+	soup = BeautifulSoup(html)
+
+	data = []
+	#data in all table rows inside div#allPlays>table
+	for tr in soup.find(id='allPlays')('tr'):
+		(period, team, eventName, eventDesc, vid) = [td.text for td in tr('td')]
+		
+		#period originally in format "1st 00:00"
+		time = period[-5:]
+		if period[0] in ('1','2','3'): period = period[0]
+		else: period = '4' #overtime
+		
+		if len(team) == 0: team = None
+
+		#period end should really have an event type
+		if eventDesc == 'period end': eventName = 'Period End'
+
+		r = IceTrackerRow()
+		r.period = period
+		r.time = time
+		r.team = team
+		r.event_type = eventName
+		r.event_description = eventDesc
+
+		data.append(r)
+
+	return data
+
+#will still have to load list of games to loop through each of them
+sampleGameId = '2015020279'
+data = getIceTrackerData(sampleGameId)
+
+for i in data:
+	print '{} {} {:3} {:10} {:55}'.format(i.period, i.time, '' if i.team == None else i.team, i.event_type, i.event_description)
