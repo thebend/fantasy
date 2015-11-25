27d9ef2f306526d1e80bbee207b0fdfaf9a6d307
Benjamin Davidson <ben.davidson@gmail.com>
2015-11-25 00:05:06 -0800

    Support for gameType
    Pre-season, regular season, playoffs

--- collection/game-summary/parser.py
+++ collection/game-summary/parser.py
@@ -1,6 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
+gameTypes = {
+	'1': 'Pre-Season',
+	'2': 'Regular Season',
+	'3': 'Playoffs'
+}
+
 reportAbbreviations = {
 	'GS': 'Game Summary',
 	'ES': 'Event Summary',
@@ -18,18 +24,22 @@ season = 2014
 seasonName = '%s%s' % (season, season+1) #eg. 20142015
 seasonGameNumber = 0
 #only break when an invalid game has been found
-while True:
-	seasonGameNumber += 1
-	gameId = '{:04d}'.format(seasonGameNumber)
-	for abbr,reportType in reportAbbreviations.iteritems():
-		#why the 02 portion? before gameid 01 does lead to a real game!
-		url = 'http://www.nhl.com/scores/htmlreports/%s/%s02%s.HTM' % (seasonName, abbr, gameId)
-		if abbr == 'GS':
-			print url
-			html = requests.get(url).text
-			soup = BeautifulSoup(html, 'html.parser')
-			(date, venue, time, gameNumber, final) = [tr.text.strip() for tr in soup.find(id='GameInfo')('tr')[3:8]]
+for gameType, gameTypeDesc in gameTypes.iteritems():
+	gameType = '{:02d}'.format(int(gameType))
+	print 'Beginning search of %s games...' % (gameTypeDesc,)
+	
+	while True:
+		seasonGameNumber += 1
+		gameId = '{:04d}'.format(seasonGameNumber)
+		for reportTypeAbbr,reportType in reportAbbreviations.iteritems():
+			#why the 02 portion? before gameid 01 does lead to a real game!
+			url = 'http://www.nhl.com/scores/htmlreports/%s/%s%s%s.HTM' % (seasonName, reportTypeAbbr, gameType, gameId)
+			if reportTypeAbbr == 'GS':
+				print url
+				html = requests.get(url).text
+				soup = BeautifulSoup(html, 'html.parser')
+				(date, venue, time, gameNumber, final) = [tr.text.strip() for tr in soup.find(id='GameInfo')('tr')[3:8]]
 
-			print date  # Wednesday, October 8, 2014
-			print time  # Start 7:15 EDT; End 9:55 EDT
-			print venue # Attendance 19,745 at Air Canada Centre
+				print date  # Wednesday, October 8, 2014
+				print time  # Start 7:15 EDT; End 9:55 EDT
+				print venue # Attendance 19,745 at Air Canada Centre
