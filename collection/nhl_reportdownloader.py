48636d111ce74d83b586519d8fc66aeb0f61de0f
thebend <ben.davidson@gmail.com>
2015-11-29 03:22:26 -0800

    Multithreaded NHL report HTML retrieval - must configure threadpool size

--- collection/nhl_reportdownloader.py
+++ collection/nhl_reportdownloader.py
@@ -1,15 +1,27 @@
 import re
 import requests
 import nhl_urlgenerator
-
+import Queue
+		
 def get_game_report_html(url):
-	response = requests.get(url)
+		print url
+		response = requests.get(url)
+		
+		# if page not found, we tried loading a game that wasn't reported
+		if response.status_code == 404:
+			return None
+		else:
+			return response.text
 	
-	# if page not found, we tried loading a game that wasn't reported
-	if response.status_code == 404:
-		return None
-	else:
-		return response.text
+def game_report_html_downloader(url_queue, response_queue):
+	while True:
+		try:
+			url = url_queue.get(block=False)
+			html = get_game_report_html(url)
+			response_queue.put(html)
+		except Queue.Empty:
+			# done once queue is empty
+			break
 
 GAMELIST_URL_FORMAT = 'http://www.nhl.com/ice/schedulebyseason.htm?season={}{}&gameType={}'
 def get_gamelist_url(season, game_type):
@@ -26,6 +38,24 @@ def get_game_numbers(html):
 def make_tuple(x):
 	return x if type(x) in (tuple, list) else (x,)
 
+def get_all_game_report_urls(report_types, seasons, game_types, starting_game_number = 0):
+	for season in seasons:
+		print 'Beginning search of %s-%s season' % (season, season + 1)
+		for game_type in game_types:
+			print 'Beginning search of %s games...' % (nhl_urlgenerator.GAMETYPE_DESCRIPTION[game_type],)
+			
+			gamelist_url = get_gamelist_url(season, game_type)
+			print gamelist_url
+			response = requests.get(gamelist_url)
+			html = response.text
+			game_numbers = get_game_numbers(html)
+			
+			target_game_numbers = sorted([i for i in game_numbers if i >= starting_game_number])
+			for game_number in target_game_numbers:
+				for report_type in report_types:
+					url = nhl_urlgenerator.get_game_report_url(season, game_type, game_number, report_type)
+					yield url
+					
 def get_all_game_report_html(
 	report_types = ('GS',),
 	seasons = (2014,),
@@ -46,27 +76,31 @@ def get_all_game_report_html(
 	report_types = make_tuple(report_types)
 	seasons = make_tuple(seasons)
 	game_types = make_tuple(game_types)
-	 
+	
+	import threading
+	
 	for i in report_types:
 		if not i in nhl_urlgenerator.REPORT_ABBREVIATION:
 			raise Exception('report_types must be one of GS,ES,FC,FS,PL,TV,TH,RO,SS')
 	
-	for season in seasons:
-		print 'Beginning search of %s-%s season' % (season, season + 1)
-		for game_type in game_types:
-			print 'Beginning search of %s games...' % (nhl_urlgenerator.GAMETYPE_DESCRIPTION[game_type],)
-			
-			gamelist_url = get_gamelist_url(season, game_type)
-			print gamelist_url
-			response = requests.get(gamelist_url)
-			html = response.text
-			game_numbers = get_game_numbers(html)
-			
-			target_game_numbers = sorted([i for i in game_numbers if i >= starting_game_number])
-			for game_number in target_game_numbers:
-				for report_type in report_types:
-					url = nhl_urlgenerator.get_game_report_url(season, game_type, game_number, report_type)
-					print url
-					html = get_game_report_html(url)
-					if html:
-						yield html
+	url_queue = Queue.Queue()
+	for url in get_all_game_report_urls(
+		report_types, seasons, game_types, starting_game_number
+	):
+		url_queue.put(url)
+	
+	urls_active = url_queue.qsize()
+	response_queue = Queue.Queue()
+	threads = []
+	threadpool_size = 10
+	for i in range(threadpool_size):
+		t = threading.Thread(target=game_report_html_downloader, args=(url_queue, response_queue))
+		t.daemon = True
+		threads.append(t)
+		t.start()
+	
+	while urls_active > 0:
+		html = response_queue.get()
+		urls_active -= 1
+		if html:
+			yield html
--- test/collection/gs.py
+++ test/collection/gs.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+start_time = datetime.now()
+
 # add project directory to python path
 import os
 import sys
@@ -8,9 +11,13 @@ from collection.gs import GameSummary
 from collection.nhl_reportdownloader import get_all_game_report_html
 
 # 0 for unlimited
-iterations = 10
+iterations = 0
 count = 0
-for report_html in get_all_game_report_html('GS',2014,3):
+for report_html in get_all_game_report_html('GS',2014,(1,2,3)):
 	print GameSummary.get_from_html(report_html)
 	count += 1
 	if count == iterations: break
+
+end_time = datetime.now()
+execution_time = end_time - start_time
+print 'Total execution time: %s' % execution_time
