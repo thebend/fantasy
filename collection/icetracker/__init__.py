41e19da15cc753486e440ea32d57d2d36a895565
thebend <ben.davidson@gmail.com>
2015-12-04 16:56:46 -0800

    Parse icetracker log

--- collection/icetracker/__init__.py
+++ collection/icetracker/__init__.py
@@ -8,29 +8,85 @@ Some events will go into their own table.
 Some will follow a generic format.
 Must develop factory to differentiate automatically
 '''
+SHOT_TYPES = set([
+	'Wrist Shot',
+	'Wrap-around',
+	'Slap Shot',
+	'Snap Shot',
+	'Backhand',
+	'Slap Shot',
+	'Tip-In',
+	'Deflected'
+])
+
+'''
+Read this thread: http://stackoverflow.com/questions/6629876/how-to-make-an-anonymous-function-in-python-without-christening-it
+Consider making use of decorators instead of this dictionary method
+May make code cleaner, would be good to use decorators for learning purposes
+'''
 def shot_processor(text):
-	return None
+	for shot_type in SHOT_TYPES:
+		if shot_type in text:
+			(shooter, goalie) = text.split(' {} '.format(shot_type))
+			goalie = goalie[9:]
+			return (shot_type, shooter, goalie)
+	raise Exception('Could not match a shot type for "{}"'.format(text))
 	
 def faceoff_processor(text):
-	return None
+	return text.split(' won against ')
 	
+PENALTY_TYPES = set([
+	'Tripping',
+	'Closing hand on puck',
+	'Hooking',
+	'Slashing'
+])
 def penalty_processor(text):
+	if ' against ' in text:
+		(text, casualty) = text.split(' against ')
+	else:
+		casualty = None
+	for penalty_type in PENALTY_TYPES:
+		if penalty_type in text:
+			aggressor = text[:-(len(penalty_type) + 2)]
+			return (aggressor, penalty_type, casualty)
 	return None
 	
 def giveaway_processor(text):
-	return None
+	return text
 	
 def takeaway_processor(text):
-	return None
+	return text
 	
 def hit_processor(text):
-	return None
+	return text.split(' hit ')
 	
 def period_end_processor(text):
 	return None
 	
 def goal_processor(text):
-	return None
+	"""
+	Parses strings like "Shea Weber (6) Slap Shot, assists: Roman Josi (9), Mike Ribeiro (9)
+	"""
+	# This function isn't particularly maintainable.
+	# Consider using regex or searching for keyword assists: none
+	processed = 0
+	players = []
+	for i,c in enumerate(text):
+		if c == '(': # start of player number
+			player = text[processed:i-1]
+			processed = i
+		elif c == ')': # end of player number
+			player_number = int(text[processed+1:i])
+			players.append((player, player_number))
+			processed = i+2
+		elif c == ':': # start of assists list
+			shot_type = text[processed:i-9]
+			processed = i+2
+	
+	shooter = players[0]
+	assists = players[1:]
+	return shooter, shot_type, assists
 	
 EVENT_PROCESSORS = {
 	'Shot': shot_processor,
@@ -49,17 +105,21 @@ class IceTrackerEventDetail():
 		self.event_type = event_type
 		self.data = EVENT_PROCESSORS[event_type](text)
 		
+	def __repr__(self):
+		return str(self.data)
+		
 class IceTrackerRow():
 	def __init__(self):
 		pass
 		
 	def __repr__(self):
-		return '{} {} {:3} {:10} {:55}'.format(
+		return '{} {} {:3} {:10} {:55}\n{}'.format(
 			self.period,
 			self.time,
 			util.nz(self.team),
 			self.event_type,
-			self.event_description
+			self.event_description,
+			self.details
 		)
 		
 def get_ice_tracker_data(gameId):
@@ -72,7 +132,7 @@ def get_ice_tracker_data(gameId):
 	data = []
 	# data in all table rows inside div#allPlays>table
 	for tr in soup.find(id='allPlays')('tr'):
-		(period, team, eventName, eventDesc, vid) = [util.clean_nbsp(td.text).strip() for td in tr('td')]
+		(period, team, eventName, eventDesc, vid) = [str(util.clean_nbsp(td.text).strip()) for td in tr('td')]
 		
 		r = IceTrackerRow()
 		period, time = period.split(' ')
--- test/collection/icetracker.py
+++ test/collection/icetracker.py
@@ -2,13 +2,8 @@ import set_path
 from collection import icetracker
 
 # will still have to load list of games to loop through each of them
-sample_game_id = '2015020279'
+sample_game_id = '2015020273'
 data = icetracker.get_ice_tracker_data(sample_game_id)
 
-types = set()
 for i in data:
-	types.add(str(i.event_type))
-	print str(i)
-
-print 'Types:'
-print ','.join(types)
+	if i.event_type == 'Goal': print str(i)
